87d60417e1de6f1f5d894a67d3b2f2e62a03980c
subixonfire <subixonfire@gmail.com>
2024-11-10 16:31:54 +0100

    Update GACS.sh

--- GACS.sh
+++ GACS.sh
@@ -88,8 +88,7 @@ run_command "wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1
 
 run_command "curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor" "Adding MongoDB key ($(( ++current_step ))/$total_steps)"
 
-run_command "echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list"
- "Adding MongoDB repository ($(( ++current_step ))/$total_steps)"
+run_command "echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list"Adding MongoDB repository ($(( ++current_step ))/$total_steps)"
 
 run_command "apt-get update -y" "Updating package list ($(( ++current_step ))/$total_steps)"
 
